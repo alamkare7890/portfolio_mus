81a939a6b6f5c54e1dc16b72db5cda453caca4cb
Masum Alam <101336554+alamkare7890@users.noreply.github.com>
2023-01-19 17:05:28 +0530

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
-let toggle = document.getElementById('toggle_menu');
-console.log(toggle);
-toggle.onclick = () =>{
-  toggle.classList.toggle('active');
-}
-
 $(document).ready(function(){
 
   $(window).scroll(function(){
